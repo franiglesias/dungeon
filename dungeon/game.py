482e2f820168d6b2b0b19d99c23e99a5970ce379
Fran Iglesias <franiglesias@mac.com>
2022-11-10 19:02:02 +0100

    Add action echoing
    The walking skeleton shows a typical game interaction

--- dungeon/game.py
+++ dungeon/game.py
@@ -12,7 +12,9 @@ class Game:
         result = "I don't understand"
         command, argument = instruction.split(" ", 1)
         if command == "go":
+            print("You said: {c} {a}".format(c=command, a=argument))
             result = self.dungeon.go(argument)
         if command == "look":
+            print("You said: {c} {a}".format(c=command, a=argument))
             result = self.dungeon.look(argument)
         return result

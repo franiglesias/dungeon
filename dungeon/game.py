716c13d62e907e0584335b753b27e70ab7fb7e8d
Fran Iglesias <franiglesias@mac.com>
2022-11-15 20:15:03 +0100

    Game can accept a created Dungeon

--- dungeon/game.py
+++ dungeon/game.py
@@ -5,8 +5,8 @@ class Game:
     def __init__(self):
         self.dungeon = None
 
-    def start(self):
-        self.dungeon = Dungeon()
+    def start(self, dungeon=Dungeon()):
+        self.dungeon = dungeon
 
     def do_command(self, command):
         return command.do(self.dungeon)

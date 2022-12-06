7b966ddb78b5c1acad227eb7496f7ed8c0db2ea9
Fran Iglesias <franiglesias@mac.com>
2022-12-06 18:46:23 +0100

    Game manages player_died and player_exited events

--- dungeon/app/game.py
+++ dungeon/app/game.py
@@ -1,5 +1,6 @@
 class Game:
     def __init__(self, player, obtain_input, printer):
+        self._finished = False
         self._player = player
         self._input = obtain_input
         self._printer = printer
@@ -11,3 +12,12 @@ class Game:
 
     def not_finished(self):
         return self._player.is_alive() and not self._player.has_won()
+
+    def notify(self, event):
+        if event.name() == "player_exited":
+            self._finished = True
+        if event.name() == "player_died":
+            self._finished = True
+
+    def finished(self):
+        return self._finished
---
+++ dungeon/tests/app/test_game.py
@@ -0,0 +1,35 @@
+import unittest
+
+from dungeon.app.domain.dungeon_factory import DungeonFactory
+from dungeon.app.domain.player.player import Player
+from dungeon.app.domain.player.player_events import PlayerExited, PlayerDied
+from dungeon.app.game import Game
+from dungeon.app.printer import Printer
+from dungeon.tests.fakes.obtain_user_command.fixed_obtain_user_command import FixedObtainUserCommand
+from dungeon.tests.fakes.show_output.fake_show_output import FakeShowOutput
+
+
+class GameTestCase(unittest.TestCase):
+    def test_game_handles_player_exited_event(self):
+        dungeon = DungeonFactory().make('test')
+        player = Player.awake()
+        player.awake_in(dungeon)
+
+        game = Game(player=Player, obtain_input=FixedObtainUserCommand("go north"), printer=Printer(FakeShowOutput()))
+        game.notify(PlayerExited())
+
+        self.assertTrue(game.finished())
+
+    def test_game_handles_player_died_event(self):
+        dungeon = DungeonFactory().make('test')
+        player = Player.awake()
+        player.awake_in(dungeon)
+
+        game = Game(player=Player, obtain_input=FixedObtainUserCommand("go north"), printer=Printer(FakeShowOutput()))
+        game.notify(PlayerDied())
+
+        self.assertTrue(game.finished())
+
+
+if __name__ == '__main__':
+    unittest.main()

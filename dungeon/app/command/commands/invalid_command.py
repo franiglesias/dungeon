4b84c9c9224d8bd1382716b69af243f370d2f637
Fran Iglesias <franiglesias@mac.com>
2023-01-13 18:20:33 +0100

    Fix name

--- dungeon/app/command/commands/invalid_command.py
+++ dungeon/app/command/commands/invalid_command.py
@@ -2,8 +2,8 @@ from dungeon.app.command.command import Command
 
 
 class InvalidCommand(Command):
-    def __init__(self, user_input):
-        super().__init__(user_input)
+    def __init__(self, argument):
+        super().__init__(argument)
 
     def __str__(self) -> str:
         return "You said: {}".format(self._argument)

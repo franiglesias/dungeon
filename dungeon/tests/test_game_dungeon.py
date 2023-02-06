6a7eae9cc097d3360862ec399ae71bdf36d86d80
Fran Iglesias <franiglesias@mac.com>
2023-02-06 09:24:51 +0100

    Starting point to introduce a dungeon resolving tester

--- dungeon/tests/test_game_dungeon.py
+++ dungeon/tests/test_game_dungeon.py
@@ -8,34 +8,53 @@ from dungeon.app.show_output import MirrorUserCommand
 from dungeon.app.toggles.toggles import Toggles
 
 
+class CommandSequenceMother:
+    def __init__(self):
+        self._dungeons = {
+            'test': [
+                "go south",
+                "go north"
+            ],
+            'game': [
+                'go north',
+                'go north',
+                'go north',
+                'go east',
+                'go north',
+                'go east',
+                'go east',
+                'go south',
+                'go west',
+                'get RedKey',
+                'go east',
+                'go south',
+                'open east',
+                'go east'
+            ]
+        }
+
+    def for_dungeon(self, name):
+        return self._dungeons[name]
+
+
 class GameDungeonTestCase(unittest.TestCase):
     def setUp(self):
         self.observer = FakeObserver()
+        dungeons = ["test", "game"]
+        self._generator = (dungeon for dungeon in dungeons)
 
-    def test_using_commands(self):
-        commands = [
-            'go north',
-            'go north',
-            'go north',
-            'go east',
-            'go north',
-            'go east',
-            'go east',
-            'go south',
-            'go west',
-            'get RedKey',
-            'go east',
-            'go south',
-            'open east',
-            'go east'
-        ]
+    def test_dungeon_can_be_resolved(self):
+        for dungeon in self._generator:
+            self.run_game_with_dungeon(dungeon)
 
+    @staticmethod
+    def run_game_with_dungeon(dungeon_to_test):
+        commands = CommandSequenceMother().for_dungeon(dungeon_to_test)
         obtain_user_command = SequenceObtainUserCommand(commands)
         show_output = MirrorUserCommand()
         toggles = Toggles()
-
         application = Application(obtain_user_command, show_output, DungeonFactory(), toggles)
-        application.run(dungeon_name='game')
+        application.run(dungeon_name=dungeon_to_test)
 
 
 if __name__ == '__main__':
